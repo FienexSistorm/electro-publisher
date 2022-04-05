9c21c26f1423b020982557ce360d394e8a06632d
Othmane FETTACHE <o.fettache@sophatel.com>
2022-04-05 13:49:25 +0000

    basic config for auto realse pushing for github

--- electro-main.js
+++ electro-main.js
@@ -1,44 +1,58 @@
-const { app,screen, BrowserWindow, globalShortcut, Menu } = require("electron");
+const { app, screen, BrowserWindow, globalShortcut, Menu, } = require("electron");
+const { autoUpdater } = require("electron-updater");
+
 const path = require("path");
 
-var mainWindow = null;
-const createWindow = () => {
+let mainWindow;
 
+
+
+//-----------------------------------------
+// -----------------LOGGING---------------
+//-----------------------------------------
+autoUpdater.logger = log;
+autoUpdater.logger.transports.file.level = 'info';
+log.info('App starting...');
+/*-------------- END OF LOGGING----------- */
+
+
+const createWindow = () => {
   // getting the size of the screen
-  const primaryDisplay = screen.getPrimaryDisplay()
-  const { width, height } = primaryDisplay.workAreaSize
+  const primaryDisplay = screen.getPrimaryDisplay();
+  const { width, height } = primaryDisplay.workAreaSize;
 
   mainWindow = new BrowserWindow({
-   width: width,
+    width: width,
     height: height,
     title: "Electropharm",
     webPreferences: {
       preload: path.join(__dirname, "preload.js"),
       webSecurity: false,
       devTools: true,
-      nodeIntegration: true
+      nodeIntegration: true,
     },
   });
-  Menu.setApplicationMenu(Menu.buildFromTemplate([{
-    label: "Refresh",
-    accelerator: "Ctrl+R",
-      click(){
-        BrowserWindow.getFocusedWindow().reload();
-        mainWindow.loadFile("dist/electronDemo/index.html");
-      }
-  }
-  ,{
-    label: "Inspect",
-    accelerator: "Shift+CommandOrControl+I",
-      click(){
-        console.log("inspected")
-        mainWindow.webContents.openDevTools();
-      }
-  }
-
-]))
+  Menu.setApplicationMenu(
+    Menu.buildFromTemplate([
+      {
+        label: "Refresh",
+        accelerator: "Ctrl+R",
+        click() {
+          BrowserWindow.getFocusedWindow().reload();
+          mainWindow.loadFile("dist/electronDemo/index.html");
+        },
+      },
+      {
+        label: "Inspect",
+        accelerator: "Shift+CommandOrControl+I",
+        click() {
+          console.log("inspected");
+          mainWindow.webContents.openDevTools();
+        },
+      },
+    ])
+  );
   mainWindow.loadFile("dist/electronDemo/index.html");
-
 
   mainWindow.on("resize", () => {
     console.log("resizing");
@@ -51,15 +65,21 @@ const createWindow = () => {
 var reload = () => {
   BrowserWindow.getFocusedWindow().reload();
   mainWindow.loadFile("dist/electronDemo/index.html");
-}
+};
 
+
+
+
+
+
+
+//// CREATING THE APPLICATION FRAME
 app.whenReady()
   .then(() => {
-    globalShortcut.register('Shift+CommandOrControl+I', () => {
-    mainWindow.webContents.openDevTools();
-  })
-  })
-  .then(() => {
+    globalShortcut.register("Shift+CommandOrControl+I", () => {
+      mainWindow.webContents.openDevTools();
+    });
+  }).then(() => {
     createWindow();
     app.on("activate", () => {
       if (BrowserWindow.getAllWindows().length === 0) createWindow();
@@ -78,20 +98,25 @@ app.on("window-all-closed", () => {
                     AUTO UPDATER
 ----------------------------------------------------*/
 
-// ipcMain.on('app_version', (event) => {
-//   event.sender.send('app_version', { version: app.getVersion() });
-// });
+function sendStatusToWindow(text) {
+  log.info(text);
+  win.webContents.send('message', text);
+}
 
-// autoUpdater.on('update-available', () => {
-//   console.log("Main, an update is available")
-//   mainWindow.webContents.send('update_available');
-// });
+ipcMain.on("app_version", (event) => {
+  event.sender.send("app_version", { version: app.getVersion() });
+});
 
-// autoUpdater.on('update-downloaded', () => {
-//   console.log("Main, and update is being downloaded")
-//   mainWindow.webContents.send('update_downloaded');
-// });
+autoUpdater.on("update-available", () => {
+  console.log("Main, an update is available");
+  mainWindow.webContents.send("update_available");
+});
 
-// ipcMain.on('restart_app', () => {
-//   autoUpdater.quitAndInstall();
-// });
+autoUpdater.on("update-downloaded", () => {
+  console.log("Main, and update is being downloaded");
+  mainWindow.webContents.send("update_downloaded");
+});
+
+ipcMain.on("restart_app", () => {
+  autoUpdater.quitAndInstall();
+});
