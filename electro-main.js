e2ea50b675c9ebf46ba1fecf4af52b94cdbe2312
Othmane FETTACHE <o.fettache@sophatel.com>
2022-04-07 12:40:51 +0000

    basic auto update example

--- electro-main.js
+++ electro-main.js
@@ -1,75 +1,81 @@
-const {
-  app,
-  BrowserWindow,
-  Menu,
-} = require('electron');
+const { app, BrowserWindow, Menu, } = require('electron');
 const log = require('electron-log');
-const {
-  autoUpdater
-} = require("electron-updater");
+const { autoUpdater } = require("electron-updater");
 
-//-------------------------------------------------------------------
 
-//-------------------------------------------------------------------
+// Defining the log
 autoUpdater.logger = log;
 autoUpdater.logger.transports.file.level = 'info';
 log.info('App starting...');
 
 
-
+// Declaring the window object
 let win;
-let progressInterval;
 
+
+/**@method sendStatusToWindow to push messages to the window (the interface html page) */
 function sendStatusToWindow(text) {
   log.info(text);
   win.webContents.send('message', text);
 }
 
-function showProgress(percent) {
-  const INTERVAL_DELAY = 100 // ms
-  progressInterval = setInterval(() => {
-    if (percent < 1) {
-      percent += INCREMENT
-      win.setProgressBar(percent);
-    } else {
-      win.setProgressBar(-1);
-      clearInterval(progressInterval)
-    }
-  }, INTERVAL_DELAY)
-
-}
-
+/**@method createDefaultWindow return the configured window object */
 function createDefaultWindow() {
+  // setting up the window
   win = new BrowserWindow({
     webPreferences: {
       nodeIntegration: true,
-      contextIsolation: false
+      contextIsolation: false,
+      devTools: true
     }
   });
 
+  // opening the devtools on lunch
   win.webContents.openDevTools();
   win.on('closed', () => {
     win = null;
   });
-  win.loadURL(`file://${__dirname}/dist/electronDemo/index.html`);
-  // win.loadFile("./dist/electronDemo/index.html");
+  win.loadURL(`file://${__dirname}/dist/loading.html`);
   return win;
 }
+
+
+app.on('ready', function () {
+  // const menu = Menu.buildFromTemplate(template);
+  // Menu.setApplicationMenu(menu);
+  createDefaultWindow();
+  console.log(app.getVersion())
+  autoUpdater.checkForUpdatesAndNotify()
+});
+
+// Handle the window closure action
+app.on('window-all-closed', () => {
+  app.quit();
+});
+
+
+
+
+/******************************************* */
+/************** AUTO UPDATER *************** */
 autoUpdater.on('checking-for-update', () => {
   sendStatusToWindow('Checking for update...');
+  win.loadURL(`file://${__dirname}/dist/loading.html`);
 })
 autoUpdater.on('update-available', (info) => {
   sendStatusToWindow('Update available.');
+  win.loadURL(`file://${__dirname}/dist/version.html`);
 })
 autoUpdater.on('update-not-available', (info) => {
   sendStatusToWindow('Update not available.');
+  win.loadURL(`file://${__dirname}/dist/electronDemo/index.html`);
 })
 autoUpdater.on('error', (err) => {
   sendStatusToWindow('Error in auto-updater. ' + err);
+  win.loadURL(`file://${__dirname}/dist/electronDemo/index.html`);
 })
 
 autoUpdater.on('download-progress', (progressObj) => {
-  showProgress(progressObj.percent);
   let log_message = "Download speed: " + progressObj.bytesPerSecond;
   log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
   log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
@@ -78,24 +84,8 @@ autoUpdater.on('download-progress', (progressObj) => {
 
 autoUpdater.on('update-downloaded', (info) => {
   sendStatusToWindow('Update downloaded');
-  autoUpdater.quitAndInstall();
+  autoUpdater.quitAndInstall(true, true);
 });
-
-app.on('ready', function () {
-  const menu = Menu.buildFromTemplate(template);
-  Menu.setApplicationMenu(menu);
-  console.log(app.getVersion())
-  createDefaultWindow();
-});
-
-app.on('window-all-closed', () => {
-  app.quit();
-});
-
-app.on('ready', function () {
-  autoUpdater.checkForUpdatesAndNotify();
-});
-
 
 /**
  * TODO:
@@ -103,5 +93,6 @@ app.on('ready', function () {
  * 2- If anupdate has beeb deteced, it should no render the index page itself, but it must show a page holding informations about the update
  * 3- (OTHER OPTION) Try using quiet install instead of showing the page of installation
  * 4- Main Objectif is to skip the installation steps in case of updates  (silent install)
- *
+ * 5- Create an update page to be displayed in case the app is in update progress mode
+ * 6- DO ALL OF THIS FOR THE MAIN APPLICATION WINPHARM
  */
